3c202b7811b8d1a3fad356431f92ae5ebe0db1aa
femi omoshona <57992168+femostic4j@users.noreply.github.com>
2021-10-07 03:28:53 -0700

    Update Lottery.sol

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,7 +1,8 @@
 
 pragma solidity ^0.4.17;
+//solidity version
 
-contract Lottery{
+contract Lottery{  //contract name declearation
     address public manager;
     address[] public players;
     
@@ -10,24 +11,24 @@ contract Lottery{
         manager = msg.sender;
     }
     function enter() public payable{
-        require(msg.value > .01 ether);
+        require(msg.value > .01 ether); //entering the pool or contract with .01 ether 
         players.push(msg.sender);   
     }
-    function random() private view returns(uint){
+    function random() private view returns(uint){  // Pseudo random number generator
         return uint(keccak256(block.difficulty, now, players));
     }
     function pickWinner() public restricted{
-        uint index = random() % players.length;
-        players[index].transfer(this.balance);
-        players = new address[](0);
+        uint index = random() % players.length; //Selecting a winner in the contract or pool
+        players[index].transfer(this.balance);  //Transfering the total ether to the winner
+        players = new address[](0);  //Resetting the state of our lottery contract after picking a winner, for new new lottery
     }
     
-    modifier restricted(){
-        require(msg.sender == manager);
+    modifier restricted(){  //declearation of new function modifier
+        require(msg.sender == manager);  
         _;
     }
     
-    function getPlayers() public view returns(address[]){
+    function getPlayers() public view returns(address[]){  //returning a list or numbeers of players who enter the contract
         return players;
     }
 }
